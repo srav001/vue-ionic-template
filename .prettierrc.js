9d800ec1ac6fdd1b71d80167ac5799ac4137904c
sravan3 <sravan@linways.com>
2021-12-19 01:19:56 +0530

    feat: added vue 3 and ionic-vue

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+	trailingComma: 'es5',
+	printWidth: 120,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: true,
+	bracketSpacing: true,
+	bracketSameLine: true
+};
